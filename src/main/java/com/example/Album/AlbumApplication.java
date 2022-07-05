9b1be63ad7cb949291a4d01a50af74690b3d613a
BasantaBabu <basantab.ghimire@gmail.com>
2022-07-05 13:15:06 -0400

    1st commit create album project

---
+++ src/main/java/com/example/Album/AlbumApplication.java
@@ -0,0 +1,14 @@
+package com.example.Album;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumApplication.class, args);
+		System.out.println("Album-Assignment -2 is running");
+	}
+
+}
